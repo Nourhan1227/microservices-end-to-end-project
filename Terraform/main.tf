23cae7dd013c6cd67e91f05433becd42af92b208
Nourhan1227 <nourashour1227@gmail.com>
2025-02-17 21:09:40 +0200

    5th  commit

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   region=var.region # "us-east-1"
-  access_key="aws-access-key"
-  secret_key="aws-secret-key"
+  # access_key="aws-access-key"
+  # secret_key="aws-secret-key"  
 }
 
 resource "aws_vpc" "my-vpc" {
